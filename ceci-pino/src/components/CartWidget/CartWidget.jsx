54540966c6b669f9fcca7e86f0f16ed9037558d4
ceciPino <mepino@uc.cl>
2023-11-02 14:37:26 -0300

    primera pre entrega con carwidget corregido

--- ceci-pino/src/components/CartWidget/CartWidget.jsx
+++ ceci-pino/src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,11 @@
-import {cart} from './assets/cart.svg'
+import cart from './assets/cart.svg'
+
 function CartWidget() {
     return (
+        
         <div>
             <img src={cart} alt="cart-widget" />
-         <span>0</span>
+            <span>0</span>
         </div>
     )
 }
